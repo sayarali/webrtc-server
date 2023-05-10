a01ca85891abbeb692ff9f94aeecc0fae32473ff
Ali Sayar <alisayar07@gmail.com>
2023-05-10 23:00:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+const http = require("http")
+const Socket = require("websocket").server
+const server = http.createServer(()=>{})
+
+server.listen(2208,()=>{
+    console.log("2208 portunda server başlatıldı")
+})
+
+const webSocket = new Socket({httpServer:server})
+
+const users = []
+
+webSocket.on('request',(req)=>{
+    const connection = req.accept()
+
+    connection.on('message',(message)=>{
+        const data = JSON.parse(message.utf8Data)
+        const user = findUser(data.name)
+
+        switch(data.type){
+            case "store_user":
+                if(user != null){
+                    connection.send(JSON.stringify({
+                        type:'kullanıcı zaten oturumda'
+                    }))
+                    return
+                }
+                const newUser = {
+                    name: data.name, conn: connection
+                }
+                users.push(newUser)
+               
+            break
+
+            case "start_call":
+                let userToCall = findUser(data.target)
+                if(userToCall){
+                    connection.send(JSON.stringify({
+                        type:"call_response", data:"kullanıcı arama için hazır"
+                    }))
+                } else {
+                    connection.send(JSON.stringify({
+                        type:"call_response", data:"kullanıcı çevrimdışı"
+                    }))
+                }
+            break
+
+            case "create_offer":
+                let userToReceiveOffer = findUser(data.target)
+
+                if(userToReceiveOffer){
+                    userToReceiveOffer.conn.send(JSON.stringify({
+                        type:"offer_received",
+                        name:data.name,
+                        data:data.data.sdp
+                    }))
+                }
+            break
+
+            case "create_answer":
+                let userToReceiveAnswer = findUser(data.target)
+
+                if(userToReceiveAnswer){
+                    userToReceiveAnswer.conn.send(JSON.stringify({
+                        type:"answer_received",
+                        name:data.name,
+                        data:data.data.sdp
+                    }))
+                }
+            break
+
+            case "ice_candidate":
+                let userToReceiveIceCandidate = findUser(data.target)
+
+                if(userToReceiveIceCandidate){
+                    userToReceiveIceCandidate.conn.send(JSON.stringify({
+                        type:"ice_candidate",
+                        name:data.name,
+                        data:{
+                            sdpMLineIndex: data.data.sdpMLineIndex,
+                            sdpMid: data.data.sdpMid,
+                            sdpCandidate: data.data.sdpCandidate
+                        }
+                    }))
+                }
+            break
+
+        }
+    })
+
+    connection.on('close', () => {
+        users.forEach( user => {
+            if(user.conn === connection){
+                users.splice(users.indexOf(user), 1)
+            }
+        })
+    })
+
+})
+
+
+const findUser = username => {
+    for(let i = 0; i<users.length; i++){
+        if(users[i].name === username){
+            return users[i]
+        }
+    }
+}
